f6f246dc15b64b1575f2189e151aa7bd4b62fc87
prabhu-durasoft <iamprabhu@yahoo.com>
2018-11-10 11:07:41 +0530

    created folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <p>
+            Decoder
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+
+const reducers = () => {
+	
+};
+
+
+const Store = createStore(reducers);
+export default Store;
